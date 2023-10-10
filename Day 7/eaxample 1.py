dbcb2dbf9c988932950ceb88f01931e5eff2e69e
Mischa MacKinnon <mmackinnon@bayviewglen.ca>
2023-10-10 14:40:54 -0400

    dots and bozes

---
+++ Day 7/eaxample 1.py
@@ -0,0 +1,25 @@
+average = 12.657
+print(f"{average:2f}") #rounds too 2 decimal place
+print(f"{avarege:3f}") #12.657
+
+number = 6.1
+print(f"{number:2f}") #6.10
+
+number = 6
+print(f"{number:2f}") #6.0
+
+math_mark = 34
+math_total = 45
+
+math_average = math_mark / math_total
+
+print(f"{math_average}") #0.75555555
+print(f"{math_average:1%}") # 75.0%
+
+x = 54.5476464646646464646647467466
+print(f"{x}")
+print(f"{x: 5f}")
+print(f"{x: 5}")
+print(f"{x: 2}")
+print(f"{x: 3}")
+print(f"{x: 1}")
