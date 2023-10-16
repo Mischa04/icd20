8f771ed40169b9ecee323752087cd75c9295ea4c
Mischa MacKinnon <mmackinnon@bayviewglen.ca>
2023-10-16 15:12:56 -0400

    changes

---
+++ Unit Two-Writing Python Functions And Variable Scope/Day 1 - introduction/using-functions-examples1.py
@@ -0,0 +1,16 @@
+length = len("Happy Cat")#Len Funcition, Takes a string and returns number of characters
+print(length) #Print dunction that takes a string or any other type and prints it to the output
+# Happy Cat is the argument that len function took, it returned the value 9
+
+mystery = print("Happy Cat")
+
+
+print(mystery)
+
+width = int(intput("Pleas enter a width: "))
+# Please enter a width is teh arhument for the input function
+#It returns what user enters
+#That will be the argument for the int function
+#The int function will convert that into an int and return which gets assigned to widthprint
+print(round(24.6766876, 2))
+print(round(34.6766876))
