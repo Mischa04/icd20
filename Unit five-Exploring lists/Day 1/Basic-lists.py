73d159cea9fc7d81f760871d88ba43cfb213d206
Mischa MacKinnon <mmackinnon@bayviewglen.ca>
2024-01-23 12:47:40 -0500

    baseball

---
+++ Unit five-Exploring lists/Day 1/Basic-lists.py
@@ -0,0 +1,42 @@
+# Var = ["Geeks", "for", "Geeks"]
+# print(Var)
+
+
+
+# Python program to demonstrate
+# Creation of List
+ 
+# # Creating a List
+# List = []
+# print("Blank List: ")
+# print(List)
+ 
+# # Creating a List of numbers
+# List = [10, 20, 14]
+# print("\nList of numbers: ")
+# print(List)
+ 
+# # Creating a List of strings and accessing
+# # using index
+# List = ["Geeks", "For", "Geeks"]
+# print("\nList Items: ")
+# print(List[0]) # starts counting at 0 and this is the first element in the list
+# print(List[2]) # print the third element in the list
+
+list = []
+list.append("hello")
+list.append("Steve")
+list.append("Abraham")
+list.append("Chicken")
+
+print(list)
+list.append(7)
+print(list)
+
+list.insert(2, "Dev")
+print(list)
+list.remove("Chicken")
+print(list)
+list.pop(1)
+print(list)
+print(who_left)
